c501168c6d3280b46fafdacb2c98cba100848df8
salafiaji <salafiaji@gmail.com>
2022-09-10 00:08:33 -0700

    Hello world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,7 +4,7 @@
 
 /**
  * main - Entry point
- * Return: 1 
+ * Return: 1
  */
 
 int main(void)
@@ -14,7 +14,7 @@ int main(void)
 	int s = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
 			);
 
-		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 
 			s);
 
 	return (1);
