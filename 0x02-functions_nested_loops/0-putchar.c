d115b6ee389668ac726fe1011818058ad2efb1fb
salafiaji <salafiaji@gmail.com>
2022-09-13 15:40:19 -0700

    Variables and conditionals

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,24 +1,17 @@
-#include <unistd.h>
-
-int _putchar(char c)
+#include "main.h"
+/**
+ * main - prints the string "_putchar" from a character array.
+ * Return: 0 on success.
+ */
+int main(void)
 {
-	return (write(1, &c, 1));
-}
+	char c[] = "_putchar";
+	int i;
 
-int main ()
-{
-
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-
-
-return 0;
-
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(c[i]);
+	}
+	_putchar('\n');
+	return (0);
 }
