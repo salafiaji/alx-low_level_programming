80ed3072a02160d9482cfabfbb360b1f9ac20aa6
salafiaji <salafiaji@gmail.com>
2022-09-13 14:57:31 -0700

    Variables and conditionals

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,17 +1,22 @@
 #include "main.h"
-
-
+/**
+ * print_alphabet_x10 - Print alphabet in lowercase 10 times
+ * Return: 0
+ */
 void print_alphabet_x10(void)
 {
-   int j = 0;
-   for (j = 0 ; j<10 ; j++)
-   {
-       char i = 'a';
-       while (i <= 'z') {
-       	 _putchar(i);
-        i++;
-   	 }
-   _putchar('\n');
-   }
+	int i = 0;
+	char c;
 
+	while (i < 10)
+	{
+		c = 'a';
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
+		i++;
+	}
 }
