84425452bc7eb404bf7bdfbcec319be67233e524
salafiaji <salafiaji@gmail.com>
2022-09-13 14:49:43 -0700

    Variables and conditionals

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,16 @@
 #include "main.h"
-
-
+/**
+ * print_alphabet - Print alphabet in lowercase using only `_putchar`
+ * Return: 0
+ */
 void print_alphabet(void)
 {
-   char i ='a';
-    while (i <= 'z') {
-        _putchar(i);
-        i ++;
-    }
-   _putchar('\n');
+	char c = 'a';
 
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
 }
