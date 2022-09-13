6c4b1a644c42b16d92fa572934459427b1113106
salafiaji <salafiaji@gmail.com>
2022-09-13 06:24:04 -0700

    Variables and conditionals

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,7 +1,11 @@
 #include "main.h"
+/**
+ * print_times_table - Print the `n` times table, starting with 0.
+ * Description: If `n` is greater than 15 or less than 0, print nothing.
+ * @n: int type number
+ */
 
-
-   void print_times_table(int n)
+void print_times_table(int n)
 {
 	int i, j, a;
 
@@ -16,7 +20,7 @@
 				_putchar(',');
 				_putchar(' ');
 
-				a= i * j;
+				a = i * j;
 
 				if (a <= 99)
 					_putchar(' ');
@@ -28,8 +32,8 @@
 					_putchar((a / 100) + '0');
 					_putchar(((a / 10)) % 10 + '0');
 				}
-				else 
-                if (a <= 99 && a >= 10)
+				else
+	if (a <= 99 && a >= 10)
 				{
 					_putchar((a / 10) + '0');
 				}
