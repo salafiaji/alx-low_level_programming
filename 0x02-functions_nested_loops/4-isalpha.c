811de7214e4246d49a422ca0f847414b80f623cb
salafiaji <salafiaji@gmail.com>
2022-09-13 05:49:32 -0700

    Variables and conditionals

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,12 +1,16 @@
 #include "main.h"
-
+/**
+ * _isalpha - Check if character is an alphabetic character.
+ * @c: type int character
+ * Return: 1 if letter, lowercase or uppercase, and 0 otherwise
+ */
 
 int _isalpha(int c)
 {
-    if(isalpha(c))
-        return (1);
-    else 
-	return (0);
+if (isalpha(c))
+    return (1);
+else
+    return (0);
 
 }
 
