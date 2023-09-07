59b84d867b9a00663e3a6483ab169b202e7ac554
salafiaji <salafiaji@gmail.com>
2023-09-07 15:37:51 -0700

    sizeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,12 +12,12 @@ int d;
 long int p;
 long long int q;
 float j;
-printf("Size of a char is: %lu byte(s).\n", (unsigned long)sizeof(c));
-printf("Size of an int is: %lu byte(s).\n", (unsigned long)sizeof(d));
-printf("Size of an long int is: %lu byte(s).\n", (unsigned long)sizeof(p));
-printf("Size of an long long int is: %lu bytes(s).\n",
+printf("Size of a char: %lu byte(s).\n", (unsigned long)sizeof(c));
+printf("Size of an int: %lu byte(s).\n", (unsigned long)sizeof(d));
+printf("Size of an long int: %lu byte(s).\n", (unsigned long)sizeof(p));
+printf("Size of an long long int: %lu bytes(s).\n",
 (unsigned long)sizeof(q));
-printf("Size of a float is: %lu bytes(s).\n", (unsigned long)sizeof(j));
+printf("Size of a float: %lu bytes(s).\n", (unsigned long)sizeof(j));
 
 return (0);
 }
