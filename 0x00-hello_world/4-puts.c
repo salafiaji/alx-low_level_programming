9ccfeea680cfcfd6e748a5b320586efc0fe14bb6
salafiaji <salafiaji@gmail.com>
2023-09-07 14:00:29 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Program prints string output
+ *
+ * Return: Always 0 (Successs)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
