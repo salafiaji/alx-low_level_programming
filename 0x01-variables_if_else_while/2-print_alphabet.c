3d1c18daf5b9f208a72108297341081191ea5eeb
salafiaji <salafiaji@gmail.com>
2023-09-10 12:11:10 +0100

    Prints alphabets in lower case

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabets in lower case
+ *
+ * Return: Always 0(Success)
+ */
+int main(void)
+{
+	char c = 'a';
+
+	for (; c <= 'z'; c++)
+	{
+	putchar(c);
+	}
+	return (0);
+}
