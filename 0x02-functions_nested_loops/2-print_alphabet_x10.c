1270c03f0a80449fa8492d9de90621f0a6a21fd4
salafiaji <salafiaji@gmail.com>
2023-09-12 15:53:49 +0100

    Prints 10x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Prints the alphabet in lowercase 10 timeis
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int j = 0;
+
+	while (j < 10)
+	{
+		char i = 'a';
+
+		while (i <= 'z')
+		{
+		_putchar(i);
+		i++;
+		}
+		j++;
+	 	_putchar('\n');
+	}
+}
