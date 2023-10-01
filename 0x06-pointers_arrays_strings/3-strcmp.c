1f1c55144a9abe5146afb15e74e87647eeeadbca
salafiaji <salafiaji@gmail.com>
2023-10-01 23:21:07 +0100

    Pointers

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * _strcmp -  function that compares two strings.
+ * @s1: character pointer
+ * @s2: second character pointer
+ *
+ * Return: 0 if both values are equal
+ */
+int _strcmp(char *s1, char *s2)
---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * cap_string - Function that capitalizes all words of a string.
+ * @str: string
+ *
+ * Return: string 
+ */
+char *cap_string(char *str)
+{
+	char dlimit[] = " \t\n,;.!?\"(){}";
+	for (int i)
+
+}
